3ecdd7937002b38d8e1a9b66f8820f59802758b7
njmdev03 <njmdev03@gmail.com>
2026-09-06 15:41:37 -0500

    Rename rl_imgui

--- build.zig
+++ build.zig
@@ -4,12 +4,12 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const raylib = b.dependency("raylib", .{});
-    const raylib_include = raylib.path("src");
+    const raylib_dep = b.dependency("raylib", .{});
+    const raylib_include = raylib_dep.path("src");
 
     const docking = true;
 
-    const cimgui_zig = b.dependency("cimgui_zig", .{
+    const cimgui_zig_dep = b.dependency("cimgui_zig", .{
         .target = target,
         .optimize = optimize,
         .no_platform = true,
@@ -20,12 +20,12 @@ pub fn build(b: *std.Build) !void {
         .link_libc = true,
     });
 
-    var cimgui_path: std.Build.LazyPath = cimgui_zig.path("dcimgui/master");
+    var cimgui_path: std.Build.LazyPath = cimgui_zig_dep.path("dcimgui/master");
 
     if (docking) {
-        cimgui_path = cimgui_zig.path("dcimgui/docking");
+        cimgui_path = cimgui_zig_dep.path("dcimgui/docking");
     } else {
-        cimgui_path = cimgui_zig.path("dcimgui/master");
+        cimgui_path = cimgui_zig_dep.path("dcimgui/master");
     }
 
     const cimgui_zig_include = cimgui_path;
@@ -36,44 +36,44 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    const rlimgui = b.dependency("rlImGui", .{});
-    const rlimgui_include = rlimgui.path("");
+    const cimgui_lib = cimgui_zig_dep.artifact("cimgui");
 
-    _ = b.addModule("rlImGui", .{
-        .root_source_file = b.path("src/rlImGui.zig"),
+    b.installArtifact(cimgui_lib);
+
+    const rl_imgui_dep = b.dependency("rlImGui", .{});
+    const rl_imgui_include = rl_imgui_dep.path("");
+
+    _ = b.addModule("rl_imgui", .{
+        .root_source_file = b.path("src/rl_imgui.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const rlimgui_cpp = b.createModule(.{
+    const rl_imgui_cpp = b.createModule(.{
         .target = target,
         .optimize = optimize,
         .link_libc = true,
         .link_libcpp = true,
     });
 
-    rlimgui_cpp.addCSourceFile(.{
-        .file = rlimgui.path("rlImGui.cpp"),
+    rl_imgui_cpp.addCSourceFile(.{
+        .file = rl_imgui_dep.path("rlImGui.cpp"),
         .flags = &.{},
     });
 
-    rlimgui_cpp.addCSourceFile(.{
-        .file = b.path("src/rlimgui_zig.cpp"),
+    rl_imgui_cpp.addCSourceFile(.{
+        .file = b.path("src/rl_imgui_zig.cpp"),
         .flags = &.{},
     });
 
-    rlimgui_cpp.addIncludePath(raylib_include);
-    rlimgui_cpp.addIncludePath(cimgui_zig_include);
-    rlimgui_cpp.addIncludePath(rlimgui_include);
+    rl_imgui_cpp.addIncludePath(raylib_include);
+    rl_imgui_cpp.addIncludePath(cimgui_zig_include);
+    rl_imgui_cpp.addIncludePath(rl_imgui_include);
 
-    const rlimgui_lib = b.addLibrary(.{
-        .name = "rlImGui",
-        .root_module = rlimgui_cpp,
+    const rl_imgui_lib = b.addLibrary(.{
+        .name = "rl_imgui",
+        .root_module = rl_imgui_cpp,
     });
 
-    b.installArtifact(rlimgui_lib);
-
-    const cimgui_lib = cimgui_zig.artifact("cimgui");
-
-    b.installArtifact(cimgui_lib);
+    b.installArtifact(rl_imgui_lib);
 }
